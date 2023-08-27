d7783e2ac391b447fc19cd4ff1298015c89c5e96
Oliver Lopez <oliverberto@gmail.com>
2023-08-27 19:18:20 -0400

    added helloWorld file

---
+++ tech-news-java-api/src/main/java/com.technews/helloWorld.java
@@ -0,0 +1,10 @@
+// java class is the blueprint that can used to create
+// new objects by using the new keyword
+public class HelloWorld {
+    // the main() resembles procees.argv from Node.js in that
+    // allows us to pass in arguments to the program
+    public static void main(String[] args) {
+        System.out.println("Hello, World!")
+    }
+}
+
